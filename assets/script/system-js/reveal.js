c0f601126de448c5acbfd1599241e7adf6658899
Vassja <v.misenko@web.de>
2025-06-30 14:57:20 +0200

    added javascript for the animation of second example on system.html

--- assets/script/system-js/reveal.js
+++ assets/script/system-js/reveal.js
@@ -1,2 +1,22 @@
 // script for second example in system.html
 
+document.querySelectorAll('.reveal-wrapper').forEach(box => {
+  box.addEventListener('click', () => {
+
+    // if the box is active, do the hide animation
+    if (box.classList.contains('active')) {
+
+        box.classList.remove('active');
+   
+        box.classList.add('closing');
+
+        setTimeout(() => {
+          box.classList.remove('closing');
+      }, 1800);
+      
+    // if the box is not active, do the show animation
+    } else if (!box.classList.contains('closing')) {
+      box.classList.add('active');
+    }
+  });
+});
